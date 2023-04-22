23d8d761774688f8d25429d29d15fbbfdcf7bd34
Manuel Vicario Pérez <manuel.vicario.perez@gmail.com>
2023-04-22 19:26:16 +0200

    dataLayer focus 1

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 import numpy as np
 import pandas as pd
 import json
-from api.serializers import ArticlesSerializer
+
 
 # Datos a modo de test
 test_datos = [0,1,2,30,34,21]
 serie = pd.Series(test_datos,name="test")
 obj = {'test1':1,'test2':2}
+
+
 
 # Data Base info Articles: 
 from .models import Articles
@@ -19,6 +21,7 @@ from .models import Image
 # Create your views here.
 
 def index(request):
+
     return render(request,'blog/index.html',{'numbers':serie,"obj_as_json":json.dumps(obj)})
 
 def base(request):
@@ -32,10 +35,12 @@ def article(request,slug,pk): # Return one single post/article
     test = slug
     # Check if able to retrieve data from data base based on slug | Nota importante: Estoy enviando siempre el texto del mismo artículo basado en la variable texto
     texto = Articles.objects.get(slug__exact="datalayer-episodio-1")
-    return render(request,'blog/article.html',{'numbers':serie,"obj_as_json":json.dumps(obj),"test":test,"texto":texto,"images":images})
+    articleDataLayer = Articles.objects.get(pk=pk)
+    return render(request,'blog/article.html',{'numbers':serie,"obj_as_json":json.dumps(obj),"test":test,"articleDataLayer":articleDataLayer,"texto":texto,"images":images,})
 
 
 def articles(request): # Display all the posts/articles
+    
     articles = Articles.objects.all()
     return render(request,'blog/articles_model.html',{'articles':articles})
     # Sustituir por serializer si queremos utilizar el API
