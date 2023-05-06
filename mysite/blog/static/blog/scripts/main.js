65081830639696f2486a65429cd85d8339e1a018
Manuel Vicario Pérez <manuel.vicario.perez@gmail.com>
2023-05-06 13:53:00 +0200

    Formuluario con evento

--- mysite/blog/static/blog/scripts/main.js
+++ mysite/blog/static/blog/scripts/main.js
@@ -1,28 +1,38 @@
 
-$(document).on('submit', '#post-form',function(e){
-    e.preventDefault();
-    $.ajax({
-        type:'POST',
-        url:'/blog/create-post/',
-        data:{
-            title:$('#title').val(),
-            description:$('#salary-ranges').val(),
-            csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
-            action: 'post'
-        },
-        success:function(json){
-            console.log(json)
-            $( "#result" ).empty().append(json.title + "-"+json.description);
-            window.dataLayer = window.dataLayer || [];
-            dataLayer.push({'field1':json.title,'field2':json.description})
-             
-            
-        },
-        error : function(xhr,errmsg,err) {
-        console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-    }
+
+
+    $(document).on('submit', '#post-form',function(e){
+    
+        e.preventDefault();
+        $.ajax({
+            type:'POST',
+            url:'/blog/create-post/',
+            data:{
+                title:$('#title').val().toLowerCase(),
+                description:$('#salary-ranges').val().toLowerCase(),
+                csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+                action: 'post'
+            },
+            success:function(json){
+                console.log(json)
+                //$( "#result" ).empty().append(json.title + "-"+json.description);
+                $( "#result" ).empty().append("Gracias por enviar su información");
+                window.dataLayer = window.dataLayer || [];
+                dataLayer.push({'field1':json.title,'field2':json.description})
+                $('#title').val('')   
+                
+            },
+            error : function(xhr,errmsg,err) {
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+        });
     });
-});
+
+
+
+
+
+
 
 
 
