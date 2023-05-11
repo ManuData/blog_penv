693fa4974b890e5ba8f80013397df292c718bc82
Manuel Vicario Pérez <manuel.vicario.perez@gmail.com>
2023-05-11 23:42:22 +0200

    accordion in test template

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     #path('base', views.base, name='base'),
     path('article/<slug:slug>/<int:pk>', views.article, name='article-detail'),
     path('articles', views.articles, name='articles'),
-    path('feedback/', views.create_post, name='form_ajax')
+    path('feedback/', views.create_post, name='form_ajax'),
+    path('test/', views.index, name='test')
+
      
 ] 
 
